3e98c30a1fea9e08145fe002bc1d04585c9a5048
MRP-rakib <rakib260.hossain@gmail.com>
2025-12-07 16:48:35 +0600

    add home route

--- online_courses/online_courses/urls.py
+++ online_courses/online_courses/urls.py
@@ -18,8 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def Home(request):
+    return JsonResponse({
+        'message':'system is ok',
+        'status':True
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home, name='home'),
     path('api/', include('api.urls'))
 ]
 
